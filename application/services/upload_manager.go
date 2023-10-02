6876b0404d7f74ce0e11bea0467520064039c6b0
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-10-02 10:09:24 -0300

    ✬ feat:  upload worker

--- application/services/upload_manager.go
+++ application/services/upload_manager.go
@@ -3,8 +3,10 @@ package services
 import (
 	"context"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 
 	"cloud.google.com/go/storage"
@@ -52,7 +54,7 @@ func (vu *VideoUpload) loadPaths() error {
 			vu.Paths = append(vu.Paths, path)
 		}
 		return nil
-	}) 
+	})
 
 	if err != nil {
 		return err
@@ -60,6 +62,53 @@ func (vu *VideoUpload) loadPaths() error {
 	return nil
 }
 
+func (vu *VideoUpload) ProcessUpload(concurrency int, doneUpload chan string) error {
+
+	in := make(chan int, runtime.NumCPU())
+	returnChannel := make(chan string)
+
+	err := vu.loadPaths()
+	if err != nil {
+		return err
+	}
+
+	uploadClient, ctx, err := getClientUpload()
+	if err != nil {
+		return err
+	}
+
+	for process := 0; process < concurrency; process++ {
+		go vu.uploadWorker(in, returnChannel, uploadClient, ctx)
+	}
+
+	go func() {
+		for x := 0; x < len(vu.Paths); x++ {
+			in <- x
+		}
+		close(in)
+	}()
+
+	for r := range returnChannel {
+		if r != "" {
+			doneUpload <- r
+			break
+		}
+	}
+}
+
+func (vu *VideoUpload) uploadWorker(in chan int, returnChan chan string, uploadClient *storage.Client, ctx context.Context) {
+	for x := range in {
+		err := vu.UploadObject(vu.Paths[x], uploadClient, ctx)
+
+		if err != nil {
+			vu.Errors = append(vu.Errors, vu.Paths[x])
+			log.Printf("error during the upload: %v. Error?: %v", vu.Paths[x], err)
+			returnChan <- err.Error()
+		}
+		returnChan <- ""
+	}
+	returnChan <- "uploaded completed"
+}
 
 func getClientUpload() (*storage.Client, context.Context, error) {
 	ctx := context.Background()
